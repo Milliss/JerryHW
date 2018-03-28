3b90129814ebc12a6e7d6ef576b7b2a1961b6466
Milliss <cal456093@gmail.com>
2018-03-28 17:06:34 +0800

    upup

---
+++ 0326/0326_hw4/0326_hw4.py
@@ -0,0 +1,6 @@
+from source import calculate
+
+print(calculate.add_int(5,3))
+print(calculate.minus_int(5,3))
+print(calculate.multi_int(5,3))
+print(calculate.div_int(5,3))
---
+++ 0326/0326_hw4/source/calculate.py
@@ -0,0 +1,9 @@
+
+def add_int(a,b):
+	return a+b
+def minus_int(a,b):
+	return a-b
+def multi_int(a,b):
+	return a*b
+def div_int(a,b):
+	return a/b
