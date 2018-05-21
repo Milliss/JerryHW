5d518ae805543d67d4758a31e4010e02c72266be
Milliss <cal456093@gmail.com>
2018-05-21 10:59:44 +0800

    update

--- proj/proj/mysite/blog/models.py
+++ proj/proj/mysite/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class graphh(models.Model):
+	namee=models.CharField(max_length=87)
+	discribe=models.CharField(max_length=87)
+	picture=models.CharField(max_length=87)
+	googleconnect=models.CharField(max_length=87)
+	suggest=models.CharField(max_length=878)
+	score=models.CharField(max_length=12)
+
+
+	def __str__(self):
+		return self.namee
--- proj/proj/mysite/blog/views.py
+++ proj/proj/mysite/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import loader
+from .models import graphh
 # Create your views here.
+def index(request):
+	mygraphh=graphh.objects.all()
+	return render_to_response('blog/blog.html',locals())
